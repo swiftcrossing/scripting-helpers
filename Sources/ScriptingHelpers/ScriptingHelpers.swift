e0aba55d408d38c388e86b99361986a278962b79
Nolan W <swiftcrossing@proton.me>
2023-06-13 12:58:10 +0900

    Initial commit

---
+++ Sources/ScriptingHelpers/ScriptingHelpers.swift
@@ -0,0 +1,36 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+extension String: Error {}
+
+@discardableResult
+public func shell(_ command: String) -> Result<String, String> {
+  let task = Process()
+  let outputPipe = Pipe()
+  let errorPipe = Pipe()
+
+  task.standardOutput = outputPipe
+  task.standardError = errorPipe
+  task.arguments = ["-c", command]
+  task.launchPath = "/bin/zsh"
+  task.standardInput = nil
+  task.launch()
+
+  let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+  let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
+  if let output = String(data: outputData, encoding: .utf8) {
+    return .success(output)
+  } else if let error = String(data: errorData, encoding: .utf8) {
+    return .failure(error)
+  }
+
+  return .failure("Unexpected Error")
+}
+
+public func print(_ message: String, verbose: Bool) {
+  if verbose {
+    print(message)
+  }
+}
